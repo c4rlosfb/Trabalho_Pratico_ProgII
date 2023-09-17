4005218c8c0d01f874439d29af530db79c4f0b7e
c4rlosfb <105569997+c4rlosfb@users.noreply.github.com>
2023-09-17 16:40:57 -0300

    fiz algo, será que ta certo?

--- arquivo.c
+++ arquivo.c
@@ -1,1 +1,30 @@
 #include <stdlib.h>
+
+/*Desenvolva as estruturas para representar os contatos de uma agenda conforme a descrição a seguir:
+1. endereço com char rua[50], int numero, char bairro[50] e char cidade[50];
+2. telefone com char ddd[2] e char numero[9];
+3. contato com char nome[50], char email[30], endereco e um array para armazenar 2 telefones um para o fixo e outro para o celular telefone[2].*/
+
+typedef struct endereco{
+    char rua[50];
+    int numero;
+    char bairro[50];
+    char cidade[50];
+}Endereco;
+
+typedef struct telefone{
+    char ddd[2];
+    char numero[9];
+}Telefone;
+
+typedef struct contato{
+    char nome[50];
+    char email[30];
+    Endereco endereco;
+    Telefone telefone[2];
+}Contato;
+
+int main(){
+    Contato contato;
+    return 0;
+}
