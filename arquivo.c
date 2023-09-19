5fa4457add96933232227481583232f1db8d3aa2
celsohd21 <141174260+celsohd21@users.noreply.github.com>
2023-09-19 10:45:36 -0300

    Atualizar o arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -4,27 +4,33 @@
 1. endereço com char rua[50], int numero, char bairro[50] e char cidade[50];
 2. telefone com char ddd[2] e char numero[9];
 3. contato com char nome[50], char email[30], endereco e um array para armazenar 2 telefones um para o fixo e outro para o celular telefone[2].*/
-
+#define LIMIT_RUA 50
+#define LIMIT_BAIRRO 50
+#define LIMIT_CIDADE 50
+#define LIMIT_DDD 2
+#define LIMIT_NUMERO 9
+#define LIMIT_NOME 50
+#define LIMIT_EMAIL 30
 typedef struct endereco{
-    char rua[50];
+    char rua[LIMIT_RUA];
     int numero;
-    char bairro[50];
-    char cidade[50];
+    char bairro[LIMIT_BAIRRO];
+    char cidade[LIMIT_CIDADE];
 }Endereco;
 
 typedef struct celular{
-    char ddd[2];
-    char numero[9]
+    char ddd[LIMIT_DDD];
+    char numero[LIMIT_NUMERO]
 }Celular;
 
 typedef struct telefone{
-    char ddd[2];
-    char numero[9];
+    char ddd[LIMIT_DDD];
+    char numero[LIMIT_NUMERO];
 }Telefone;
 
 typedef struct contato{
-    char nome[50];
-    char email[30];
+    char nome[LIMIT_NOME];
+    char email[LIMIT_EMAIL];
     Endereco endereco;
     celular Celular[2];
     Telefone telefone[2];
