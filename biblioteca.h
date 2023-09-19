e2282eaa6a73dd8571178ffe5e8d0d9d1ff19ba5
celsohd21 <141174260+celsohd21@users.noreply.github.com>
2023-09-19 11:34:21 -0300

    adicionei arquivo de como iniciar uma biblioteca

---
+++ biblioteca.h
@@ -0,0 +1,16 @@
+/*Fazer uso de 2 bibliotecas uma para armazenar as estruturas e outra para armazenar as funções das listas.
+Deixar apenas a função main() com o menu de opções com switch/case no arquivo principal. Faça o #include
+das bibliotecas no arquivo principal.
+1. No arquivo de cabeçalho (header le .h) vão as declarações de funções (assinaturas) e denições de
+estruturas;
+2. No arquivo de unidade (arquivo .c) vão as denições de funções (corpo da função);
+3. Então, para usar as funções, é necessário incluir o arquivo de cabeçalho (.h) com o uso de aspas duplas
+( ).
+4. Para compilar, é necessário incluir o arquivo de unidade (.c) com o uso de aspas duplas ( ).*/
+
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+
+
+#endif
